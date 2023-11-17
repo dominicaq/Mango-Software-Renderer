0b323b65ade42831b96403124c4aef43cdb4f2cd
Jared <jareddocg@gmail.com>
2023-11-16 19:43:14 -0800

    carrgie prep

--- Mango/src/Mango/mango.c
+++ Mango/src/Mango/mango.c
@@ -1,36 +1,117 @@
-#include "mango.h"
-#include <SDL.h>
 #include <time.h>
 
+#ifndef RISCV_CONSOLE
+#include <SDL.h>
+#endif
+
+#include "mango.h"
 #include "render/drawing.h"
 #include "render/framedata.h"
 
+#ifdef RISCV_CONSOLE
+uint32_t GetTicks(void);
+uint32_t GetController(void);
+uint32_t SetVideoCallback(void (*on_video)());
+
+uint32_t mango_get_controller() { return GetController(); }
+#endif
 void (*user_update)(MangoReal dt);
 void mango_on_update(void (*callback)(MangoReal dt)) { user_update = callback; }
 
+typedef struct {
+    Scene *scene;
+    Camera *camera;
+    clock_t last_time;
+    Frame *frame;
+    UBO ubo;
+} Mango;
+
+void mango_update(Mango *mango) {
+    clock_t current_time = clock();
+    double dt = current_time - mango->last_time;
+    user_update(dt);
+    mango->last_time = current_time;
+    frame_reset(mango->frame);
+
+    mango->ubo.u_cam_pos = mango->camera->game_object.position;
+
+    // Update scene transforms
+    scene_update_matrices(mango->scene);
+    camera_update_matrix(mango->camera);
+    // idealy later we here we will organize scene into bvh and then add
+    // and remove lights from a light stack ad we render objects
+
+    // Update MVP Matrix: projection * view * model (multiplication
+    // order)
+    Mat4 projection_matrix = perspective(mango->camera);
+    Mat4 view_matrix = mat4_inverse(mango->camera->game_object.world_matrix);
+    for (int i = 0; i < mango->scene->object_count; ++i) {
+        if (mango->scene->attributes[i].type != ATTR_MESH) {
+            continue;
+        }
+        Mesh *target_mesh = &mango->scene->attributes[i].mesh;
+        GameObject *target_object = mango->scene->objects + i;
+
+        // Update matricies
+        Mat4 model_matrix = target_object->world_matrix;
+        Mat4 model_view_matrix = mat4_mul(view_matrix, model_matrix);
+        Mat4 mvp = mat4_mul(projection_matrix, model_view_matrix);
+        Mat4 vp = mat4_mul(projection_matrix, view_matrix);
+
+        // Update UBO
+        mango->ubo.u_mvp = mvp;
+        mango->ubo.u_vp_inv = mat4_inverse(vp);
+        mango->ubo.u_model_view = model_view_matrix;
+        mango->ubo.u_color = target_mesh->color;
+
+        // UBO debug options
+        draw_mesh(mango->frame, target_mesh, &mango->ubo);
+    }
+
+    // TODO: Temp
+    SDFSphere dummy_sphere;
+    dummy_sphere.position =
+        (Vec3){{mango->frame->width / 2, mango->frame->height / 2, 0}};
+    dummy_sphere.radius = 50.0f;
+    dummy_sphere.color = (Vec3){{255.0f, 255.0f, 255.0f}};
+    draw_sdf(mango->frame, &dummy_sphere);
+
+    frame_update(mango->frame);
+}
+
 void mango_run(Scene *scene, Camera *camera) {
-    Frame *frame = frame_alloc(camera->width, camera->height);
-    if (frame == NULL) {
+    Mango mango;
+    mango.camera = camera;
+    mango.scene = scene;
+    mango.frame = frame_alloc(camera->width, camera->height);
+    if (mango.frame == NULL) {
         return;
     }
 
-    UBO ubo;
-    ubo.debug = scene->debug;
-    SDL_Event e;
-    bool quit = false;
+    mango.ubo.debug = scene->debug;
     clock_t start_time = clock();
-    clock_t last_time = start_time;
+    mango.last_time = start_time;
 
-    // populate lights
-    ubo.num_lights = 0;
+    mango.ubo.num_lights = 0;
     for (int i = 0; i < scene->object_count; ++i) {
         if (scene->attributes[i].type == ATTR_LIGHT) {
-            ubo.lights[ubo.num_lights] = &scene->attributes[i].light;
-            ubo.light_objects[ubo.num_lights] = &scene->objects[i];
-            ++ubo.num_lights;
+            mango.ubo.lights[mango.ubo.num_lights] =
+                &scene->attributes[i].light;
+            mango.ubo.light_objects[mango.ubo.num_lights] = &scene->objects[i];
+            ++mango.ubo.num_lights;
         }
     }
 
+#ifdef RISCV_CONSOLE
+    SetVideoCallback(mango_update);
+    SetTimerCallback(onTimer, 10000);
+
+    while (1) {
+    }
+#else
+    // populate lights
+    SDL_Event e;
+    bool quit = false;
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
@@ -41,56 +122,9 @@ void mango_run(Scene *scene, Camera *camera) {
                 break;
             }
         }
-
-        clock_t current_time = clock();
-        double dt = current_time - last_time;
-        user_update(dt);
-        last_time = current_time;
-        frame_reset(frame);
-
-        ubo.u_cam_pos = camera->game_object.position;
-
-        // Update scene transforms
-        scene_update_matrices(scene);
-        camera_update_matrix(camera);
-        // idealy later we here we will organize scene into bvh and then add
-        // and remove lights from a light stack ad we render objects
-
-        // Update MVP Matrix: projection * view * model (multiplication
-        // order)
-        Mat4 projection_matrix = perspective(camera);
-        Mat4 view_matrix = mat4_inverse(camera->game_object.world_matrix);
-        for (int i = 0; i < scene->object_count; ++i) {
-            if (scene->attributes[i].type != ATTR_MESH) {
-                continue;
-            }
-            Mesh *target_mesh = &scene->attributes[i].mesh;
-            GameObject *target_object = scene->objects + i;
-
-            // Update matricies
-            Mat4 model_matrix = target_object->world_matrix;
-            Mat4 model_view_matrix = mat4_mul(view_matrix, model_matrix);
-            Mat4 mvp = mat4_mul(projection_matrix, model_view_matrix);
-            Mat4 vp = mat4_mul(projection_matrix, view_matrix);
-
-            // Update UBO
-            ubo.u_mvp = mvp;
-            ubo.u_vp_inv = mat4_inverse(vp);
-            ubo.u_model_view = model_view_matrix;
-            ubo.u_color = target_mesh->color;
-
-            // UBO debug options
-            draw_mesh(frame, target_mesh, &ubo);
-        }
-
-        // TODO: Temp
-        SDFSphere dummy_sphere;
-        dummy_sphere.position = (Vec3){{frame->width / 2, frame->height / 2, 0}};
-        dummy_sphere.radius = 50.0f;
-        dummy_sphere.color = (Vec3){{255.0f, 255.0f, 255.0f}};
-        draw_sdf(frame, &dummy_sphere);
-
-        frame_update(frame);
+        mango_update(&mango);
     }
-    frame_free(frame);
+#endif
+
+    frame_free(mango.frame);
 }
--- cartrige/src/main.c
+++ cartrige/src/main.c
@@ -1,1 +1,82 @@
+#include <stdbool.h>
+#include <stdint.h>
 
+uint32_t GetTicks(void);
+uint32_t GetController(void);
+uint32_t SetTimerCallback(void *, uint32_t);
+uint32_t SetVideoCallback(void *);
+
+volatile int global = 42;
+volatile uint32_t controller_status = 0;
+
+volatile uint8_t *MEDIUM_DATA = (volatile uint8_t *)(0x500D0000);
+volatile uint32_t *MEDIUM_PALETTE = (volatile uint32_t *)(0x500F2000);
+volatile uint32_t *MEDIUM_CONTROL = (volatile uint32_t *)(0x500F5F00);
+
+volatile uint8_t *LARGE_DATA = (volatile uint8_t *)(0x50090000);
+#define LARGE_PALETTE ((volatile uint32_t *)(0x500F1000))
+#define LARGE_CONTROL ((volatile uint32_t *)(0x500F5B00))
+
+volatile uint32_t *MODE_REGISTER = (volatile uint32_t *)(0x500F6780);
+
+uint32_t MediumControl(uint8_t palette, int16_t x, int16_t y, uint8_t z,
+                       uint8_t index);
+
+void onVideo() {
+    static uint16_t x_pos = 0;
+    static uint16_t y_pos = 0;
+
+    // x position of square automatically moves right per frame.
+    if (x_pos > 512 - 64) {
+        x_pos = 0;
+    }
+    x_pos++;
+
+    // y position of square controlled by user.
+    uint32_t controller_status = (*((volatile uint32_t *)0x40000018));
+
+    *(uint32_t *)0x500F5B00 = MediumControl(0, x_pos, 0, 0, 0);
+}
+
+void onTimer() {
+    static bool a = false;
+    if (a)
+        LARGE_PALETTE[1] = 0xFF00FF00;
+    else
+        LARGE_PALETTE[1] = 0xFF00FFFF;
+    a = !a;
+}
+
+volatile char *VIDEO_MEMORY = (volatile char *)(0x50000000 + 0xF4800);
+int main() {
+    int last_global = 42;
+
+    // Fill out sprite data
+    for (int y = 0; y < 64; y++) {
+        for (int x = 0; x < 64; x++) {
+            *((uint8_t *)0x50090000 + (y * 64 + x)) = 1;
+        }
+    }
+
+    LARGE_PALETTE[1] = 0xFFFF0000;  // A R G B
+    *(uint32_t *)0x500F5B00 = MediumControl(0, 0, 0, 0, 0);
+    *MODE_REGISTER = 1;
+    SetVideoCallback(onVideo);
+    SetTimerCallback(onTimer, 10000);
+
+    while (1) {
+        uint32_t global = GetTicks();
+        if (global != last_global) {
+            last_global = global;
+        }
+    }
+
+    return 0;
+}
+
+uint32_t MediumControl(uint8_t palette, int16_t x, int16_t y, uint8_t z,
+                       uint8_t index) {
+    return (((uint32_t)index) << 24) | (((uint32_t)z) << 21) |
+           (((uint32_t)y + 64) << 12) | (((uint32_t)x + 64) << 2) |
+           (palette & 0x3);
+}
