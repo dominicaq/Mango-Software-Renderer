8d4c411439ebe499e6f21f3c4de07dc067a613b0
abhi <abso@ucdavis.edu>
2023-11-26 20:11:30 -0800

    doc 2

--- Mango/src/Mango/system/system.h
+++ Mango/src/Mango/system/system.h
@@ -8,6 +8,89 @@
 #ifdef RISCV_CONSOLE
 
 /*
+    Project Proposals notes
+
+    Group 1: The design of the security_check API is commendable. It
+    demonstrates a clear understanding of security essentials in operating
+    systems, especially in handling external data sources like cartridges. This
+    function's ability to check for malicious data and return specific details
+    about potential security issues in files is a vital feature for any
+    operating system.
+
+    Group 2: For the graphics mode operations, it seems the project heavily
+    relies on manual control through the controller inputs. While this is great
+    for interactive purposes, it might be beneficial to include automated
+    testing features or scripts that can simulate these inputs. This would not
+    only aid in debugging but also in demonstrating the functionality of your
+    application in environments where a physical controller might not be
+    available. Additionally, expanding the variety of graphics that can be
+    manipulated beyond a pink square to include other shapes and colors would
+    allow for more creative and diverse application development.
+
+    Group 3: The detailed Graphics API is impressive, particularly the functions
+    like drawPixel, createCanvas, and setCellType. These functions allow for a
+    high degree of customization and flexibility in game design, essential for
+    creating a visually engaging 2D maze game. The ability to manipulate
+    individual pixels and canvas elements will enable developers to create
+    intricate and dynamic game environments. 
+
+    Group 4: The introduction of a new Graphics API, including functions like
+    set_background and init_sprites_palettes, is a great addition. This could be
+    further enhanced by including 3D graphics support or more advanced rendering
+    techniques, which would allow developers to create more visually engaging
+    and dynamic applications.
+
+    Group 5: The APIs for controller management, such as addController and
+    deleteController, are well thought out. Expanding these to include support
+    for newer input devices or more complex input schemes, like touch gestures
+    or VR controllers, could further enhance the system's interactivity.
+
+    Group 6: The focus on providing a single-threaded system initially, with
+    plans to extend to a multithreaded system, demonstrates a pragmatic approach
+    to system development. This allows for a solid foundation to be established
+    before introducing the complexities of concurrency. Additionally, the
+    emphasis on abstractions, such as memory manager and device manager, is
+    excellent for ensuring system robustness and security.
+
+    Group 7: For the Video Controller API, including advanced features such as
+    support for 3D graphics, hardware acceleration, or shader programming could
+    enhance the system's capabilities for more graphically demanding games. 
+
+    Group 8: The detailed approach to threading, including mechanisms for
+    creation, termination, and process identification, shows a robust
+    understanding of concurrent programming essentials. This is crucial for a
+    gaming-focused operating system that may need to handle multiple processes
+    simultaneously. Additionally, the focus on memory management through malloc,
+    realloc, and free functions demonstrates a solid foundation for efficient
+    resource utilization.
+
+    Group 9: The focus on graphics, with APIs for loading and rendering sprites
+    and backgrounds, is well-targeted towards game development. Enhancing these
+    APIs to support more complex graphical operations, such as animations, 3D
+    rendering, or shader programming, could significantly broaden the range of
+    games that can be developed on this platform.
+
+    Group 10: The introduction of time management APIs like timeStart and
+    timeEnd is a positive step towards enabling developers to measure and manage
+    execution times of processes or tasks. Enhancing these APIs to support more
+    granular time measurements or integrating them with profiling tools could
+    offer more insights for performance optimization.
+
+    Our OS only writes to the background data as a pixel buffer to render the
+    scene, and we don't rely on threads or anything, so the advanced features
+    and APIs implemented by some of the operating systems aren't very useful for
+    us. We'll just pick the operating system that seems the most stable and best
+    documented when we're ready to port. Group 3's looks promising as of now,
+    and will only require changing the calls to get_controller,
+    get_pixel_bg_data, set_pixel_bg_controls, get_bg_palette.
+
+
+
+*/
+
+/*
+    Summary
+
     Our OS is designed for single-player 3D games, so it doesn't provide
     functions for sprites. Our 3D game interacts with the display by writing
     directly to background data pixel buffer. Our game's renderer is supported
@@ -16,6 +99,12 @@
     is really slow, especially in the rasterized and lit modes, so we needed as
     much CPU time allocated to the renderer as possible. With this goal, our OS
     doesn't have upcalls for input or interrupts for scheduling CPU threads.
+
+    The U key enables rasterized rendering, but this is significantly slower
+    than wireframe mode and the game might appear frozen or broken. It's not,
+    it's just slow.
+
+    The whole API was revised to minimize overhead of the OS, so every function is new  .
 */
 
 // "Real" is a type representing decimal numbers, either as floats or as fixed
@@ -37,8 +126,8 @@ uint32_t set_mode(uint32_t mode);
 uint32_t get_controller();
 
 // Returns pointer to pixel buffer of the background image with the associated
-// index. Valid indexes: 1-4 The grame index contains 512x288 8-bit indexes into
-// the corresponding color palette
+// index. Valid indexes: 1-4 The pixel buffer contains 512x288 8-bit indicies
+// into the corresponding color palette.
 uint32_t get_pixel_bg_data(uint32_t index);
 
 // Sets the control register of the background image with the associated index.
